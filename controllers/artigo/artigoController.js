cbcda3e67043917bc5ed74ccf95cb632f74bc193
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-12 17:09:54 -0300

    Deletando os artigos

--- controllers/artigo/artigoController.js
+++ controllers/artigo/artigoController.js
@@ -4,35 +4,47 @@ const Categoria = require("../categoria/Categoria")
 const Artigo = require("./Artigo")
 const slugify = require("slugify")
 
-router.get("/admin/artigos",(req,res)=>{
+router.get("/admin/artigos", (req, res) => {
     Artigo.findAll({
-        include:[{model:Categoria}]
-    }).then(artigos=>{
-        res.render("admin/artigos/index",{artigos:artigos})
+        include: [{ model: Categoria }]
+    }).then(artigos => {
+        res.render("admin/artigos/index", { artigos: artigos })
     })
 })
 
-router.get("/admin/artigos/novo",(req,res)=>{
-    Categoria.findAll().then(categorias=>{
-        res.render("admin/artigos/new",{
-            categorias:categorias
+router.get("/admin/artigos/novo", (req, res) => {
+    Categoria.findAll().then(categorias => {
+        res.render("admin/artigos/new", {
+            categorias: categorias
         })
     })
 })
 
-router.post("/admin/artigos/salvar",(req,res)=>{
-    let titulo = req.body.titulo 
+router.post("/admin/artigos/salvar", (req, res) => {
+    let titulo = req.body.titulo
     let conteudo = req.body.conteudo
     let categoria = req.body.categoria
 
     Artigo.create({
-        titulo:titulo,
-        slug:slugify(titulo),
-        conteudo:conteudo,
-        categoriaId:categoria
-    }).then(()=>{
+        titulo: titulo,
+        slug: slugify(titulo),
+        conteudo: conteudo,
+        categoriaId: categoria
+    }).then(() => {
         res.redirect("/admin/artigos")
     })
 })
+
+router.post("/admin/artigos/deletar", (req, res) => {
+
+    let id = req.body.id
+
+    Artigo.destroy({
+        where:{id:id}
+    }).then(()=>{
+        res.redirect("/admin/artigos")
+    })
+
+ })
 
 module.exports = router
