3597796a0d273e9f0b989926db5e2495289618ba
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-12 14:36:24 -0300

    Criando a conexão com o banco de dados

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express")
 const app = express()
 const PORTA = 8080
 const bodyParser = require("body-parser")
+const connection = require("./database/database")
+connection.authenticate().then(()=>{console.log("Banco de dados conectado com sucesso")}).catch(erro=>{console.log(`Ocorreu um erro: ${erro}`)})
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 app.use(express.static("public"))
