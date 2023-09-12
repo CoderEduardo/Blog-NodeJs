0ca7aab88ebe01ea6dbe51ed4bd20d1870f5b0da
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-12 16:08:21 -0300

    Criando o model de artigos e seu relacionamento

---
+++ controllers/artigo/Artigo.js
@@ -0,0 +1,24 @@
+const Sequelize = require("sequelize")
+const connection = require("../../database/database")
+const Categoria = require("../categoria/Categoria")
+
+const Artigo = connection.define("artigos",{
+    titulo:{
+        type:Sequelize.STRING,
+        allowNull:false
+    },
+    slug:{
+        type:Sequelize.STRING,
+        allowNull:false
+    },
+    conteudo:{
+        type:Sequelize.TEXT,
+        allowNull:false
+    }
+})
+
+Artigo.belongsTo(Categoria)
+Artigo.sync({force:false})
+
+module.exports = Artigo
+
