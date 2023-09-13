e9b022fc34a8db2526ba0708f69e8c4e61e20979
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-13 10:55:26 -0300

    Criando o model de usuarios

---
+++ controllers/usuario/Usuario.js
@@ -0,0 +1,17 @@
+const Sequelize = require("sequelize")
+const connection = require("../../database/database")
+
+const Usuario = connection.define("usuarios",{
+    email:{
+        type:Sequelize.STRING,
+        allowNull:false
+    },
+    senha:{
+        type:Sequelize.STRING,
+        allowNull:false
+    }
+})
+
+Usuario.sync({force:false})
+
+module.exports = Usuario
