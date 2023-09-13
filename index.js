a5e98cd5a669ee544164c6e6784f257f6d76b6bd
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-12 21:34:09 -0300

    Exibindo os Artigos por categoria em sua determidada página

--- index.js
+++ index.js
@@ -3,38 +3,56 @@ const app = express()
 const PORTA = 8080
 const bodyParser = require("body-parser")
 const connection = require("./database/database")
-connection.authenticate().then(()=>{console.log("Banco de dados conectado com sucesso")}).catch(erro=>{console.log(`Ocorreu um erro: ${erro}`)})
+connection.authenticate().then(() => { console.log("Banco de dados conectado com sucesso") }).catch(erro => { console.log(`Ocorreu um erro: ${erro}`) })
 const Categoria = require("./controllers/categoria/Categoria")
 const Artigo = require("./controllers/artigo/Artigo")
 const categoriaController = require("./controllers/categoria/categoriaController")
 const artigoController = require("./controllers/artigo/artigoController")
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static("public"))
-app.set("view engine",'ejs')
+app.set("view engine", 'ejs')
 
-app.get("/",(req,res)=>{
-    Artigo.findAll().then(artigos=>{
-        res.render("index",{artigos:artigos})
+app.get("/", (req, res) => {
+    Categoria.findAll().then(categorias => {
+        Artigo.findAll().then(artigos => {
+            res.render("index", { artigos: artigos, categorias: categorias })
+        })
     })
 })
 
-app.get("/:slug",(req,res)=>{
+app.get("/:slug", (req, res) => {
     let slug = req.params.slug
-    Artigo.findOne({
-        where:{slug:slug}
-    }).then(artigo =>{
-        if(artigo != undefined){
-            res.render("article",{artigo:artigo})
-        }else{
-            res.redirect("/")
-        }
+    Categoria.findAll().then(categorias => {
+        Artigo.findOne({
+            where: { slug: slug }
+        }).then(artigo => {
+            if (artigo != undefined) {
+                res.render("article", { artigo: artigo,categorias:categorias })
+            } else {
+                res.redirect("/")
+            }
+        })
     })
 })
 
-app.use("/",categoriaController)
-app.use("/",artigoController)
+app.get("/categorias/:slug",(req,res)=>{
+    let slug = req.params.slug
 
-app.listen(PORTA,()=>{
+    Categoria.findOne({
+        where:{slug:slug},
+        include:[{model:Artigo}]
+    }).then(categoria=>{
+        Categoria.findAll().then(categorias=>{
+            res.render("index",{artigos:categoria.artigos,categorias:categorias})
+        })
+    })
+
+})
+
+app.use("/", categoriaController)
+app.use("/", artigoController)
+
+app.listen(PORTA, () => {
     console.log(`Servidor Rodando na porta ${PORTA}`)
 })
