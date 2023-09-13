28850af647fcba701fae9622cf4ff3465636c03d
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-13 15:41:58 -0300

    Funcionalidade de atualizar o email adicionada

--- controllers/usuario/usuarioController.js
+++ controllers/usuario/usuarioController.js
@@ -4,17 +4,17 @@ const Usuario = require("./Usuario")
 const bcryptjs = require("bcryptjs")
 const adminAuth = require("../../middlewares/adminAuth")
 
-router.get("/admin/usuarios", adminAuth,(req, res) => {
+router.get("/admin/usuarios", adminAuth, (req, res) => {
     Usuario.findAll().then(usuarios => {
         res.render("admin/usuarios/index", { usuarios: usuarios })
     })
 })
 
-router.get("/admin/usuario/novo",(req, res) => {
+router.get("/admin/usuario/novo", (req, res) => {
     res.render("admin/usuarios/create")
 })
 
-router.post("/admin/usuario/registrar",(req, res) => {
+router.post("/admin/usuario/registrar", (req, res) => {
     let email = req.body.email
     Usuario.findOne({
         where: { email: email }
@@ -36,53 +36,133 @@ router.post("/admin/usuario/registrar",(req, res) => {
     })
 })
 
-router.get("/admin/usuario/login",(req,res)=>{
+router.get("/admin/usuario/login", (req, res) => {
     res.render("admin/usuarios/login")
 })
 
-router.post("/admin/usuario/autenticacao",(req,res)=>{
+router.post("/admin/usuario/autenticacao", (req, res) => {
     let email = req.body.email
     let senha = req.body.senha
     Usuario.findOne({
-        where:{email:email}
-    }).then(usuario=>{
-        if(usuario!=undefined){
-            let correto = bcryptjs.compareSync(senha,usuario.senha) //Aqui onde é feito a verificação da senha
+        where: { email: email }
+    }).then(usuario => {
+        if (usuario != undefined) {
+            let correto = bcryptjs.compareSync(senha, usuario.senha) //Aqui onde é feito a verificação da senha
 
-            if(correto){
-                req.session.usuario={
-                    id:usuario.id,
-                    email:usuario.email
+            if (correto) {
+                req.session.usuario = {
+                    id: usuario.id,
+                    email: usuario.email
                 }
                 res.redirect("/admin/usuarios")
-            }else{
+            } else {
                 res.redirect("/admin/usuario/login")
             }
-        }else{
+        } else {
             res.redirect("/admin/usuario/login")
         }
-    }).catch(()=>{
+    }).catch(() => {
         res.redirect("/admin/usuario/login")
     })
 })
 
-router.get("/admin/usuario/deletar",adminAuth,(req,res)=>{
+router.get("/admin/usuario/deletar", adminAuth, (req, res) => {
     Usuario.findOne({
-        where:{email:req.session.usuario.email}
-    }).then(usuario=>{
-        res.render("admin/usuarios/deletar",{usuario:usuario})
+        where: { email: req.session.usuario.email }
+    }).then(usuario => {
+        res.render("admin/usuarios/deletar", { usuario: usuario })
     })
 })
 
-router.post("/admin/usuario/deletar/conta",adminAuth,(req,res)=>{
-    let id = req.body.id 
+router.post("/admin/usuario/deletar/conta", adminAuth, (req, res) => {
+    let id = req.body.id
     let senha = req.body.senha
 
-    Usuario.findByPk(id).then(usuario=>{
-        if(usuario != undefined){
+    Usuario.findByPk(id).then(usuario => {
+        if (usuario != undefined) {
+            let correto = bcryptjs.compareSync(senha, usuario.senha)
+            if (correto) {
+                Usuario.destroy({
+                    where: { id: id }
+                }).then(() => {
+                    res.redirect("/admin/usuarios")
+                }).catch(() => {
+                    res.redirect("/admin/usuarios")
+                })
+            } else {
+                res.redirect("/admin/usuarios")
+            }
+        } else {
+            res.redirect("/admin/usuarios")
+        }
+    })
+})
+
+router.get("/admin/usuario/atualizar", (req, res) => {
+    Usuario.findOne({
+        where: { email: req.session.usuario.email }
+    }).then(usuario => {
+        res.render("admin/usuarios/atualizar", { usuario: usuario })
+    })
+})
+
+router.get("/admin/usuario/atualizar/senha_", (req, res) => {
+    Usuario.findOne({
+        where: { email: req.session.usuario.email }
+    }).then(usuario => {
+        res.render("admin/usuarios/atualizarSenha", { usuario: usuario })
+    })
+})
+
+router.post("/admin/usuario/atualizar/senha", (req, res) => {
+    let senha1 = req.body.senha1
+    let senha2 = req.body.senha2
+    let id = req.body.id
+    let senhaFinal = ""
+
+    if (senha1 == senha2) {
+        Usuario.findByPk(id).then(usuario => {
+            let senha = req.body.senha
+            let correto = bcryptjs.compareSync(senha, usuario.senha)
+            if (correto) {
+                senhaFinal = senha1
+                let salt = bcryptjs.genSaltSync(10)
+                let hash = bcryptjs.hashSync(senhaFinal, salt)
+                Usuario.update({
+                    senha: hash
+                }, { where: { id: id } }).then(() => {
+                    res.redirect("/admin/usuarios")
+                })
+            } else {
+                res.redirect("/admin/usuario/atualizar/senha_")
+            }
+        }).catch(() => {
+            res.redirect("/admin/usuarios")
+        })
+    } else {
+        res.redirect("/admin/usuarios")
+    }
+})
+
+router.get("/admin/usuario/atualizar/email_", (req, res) => {
+    Usuario.findOne({
+        where: { email: req.session.usuario.email }
+    }).then(usuario => {
+        res.render("admin/usuarios/atualizarEmail", { usuario: usuario })
+    })
+})
+
+router.post("/admin/usuario/atualizar/email", (req, res) => {
+    let id = req.body.id
+    Usuario.findByPk(id).then(usuario => {
+        if (usuario != undefined) {
+            let senha = req.body.senha
             let correto = bcryptjs.compareSync(senha,usuario.senha)
             if(correto){
-                Usuario.destroy({
+                let email = req.body.email
+                Usuario.update({
+                    email:email
+                },{
                     where:{id:id}
                 }).then(()=>{
                     res.redirect("/admin/usuarios")
@@ -95,52 +175,12 @@ router.post("/admin/usuario/deletar/conta",adminAuth,(req,res)=>{
         }else{
             res.redirect("/admin/usuarios")
         }
-    })
-})
-
-router.get("/admin/usuario/atualizar",(req,res)=>{
-    Usuario.findOne({
-        where:{email:req.session.usuario.email}
-    }).then(usuario=>{
-        res.render("admin/usuarios/atualizar",{usuario:usuario})
-    })
-})
-
-router.get("/admin/usuario/atualizar/senha_",(req,res)=>{
-    Usuario.findOne({
-        where:{email:req.session.usuario.email}
-    }).then(usuario=>{
-        res.render("admin/usuarios/atualizarSenha",{usuario:usuario})
-    })
-})
-
-router.post("/admin/usuario/atualizar/senha",(req,res)=>{
-    let senha1 = req.body.senha1
-    let senha2 = req.body.senha2
-    let id = req.body.id
-    let senhaFinal = ""
-
-    if(senha1==senha2){
-        Usuario.findByPk(id).then(usuario=>{
-        let senha = req.body.senha 
-        let correto = bcryptjs.compareSync(senha,usuario.senha)
-            if(correto){
-                senhaFinal = senha1
-                let salt = bcryptjs.genSaltSync(10)
-                let hash = bcryptjs.hashSync(senhaFinal,salt)
-                Usuario.update({
-                    senha:hash
-                },{where:{id:id}}).then(()=>{
-                    res.redirect("/admin/usuarios")
-                })
-            }else{
-                res.redirect("/admin/usuario/atualizar/senha_")
-            }
-        }).catch(()=>{
-            res.redirect("/admin/usuarios")
-        })
-    }else{
+    }).catch(()=>{
         res.redirect("/admin/usuarios")
-    }
+    })
+
+
+
 })
+
 module.exports = router
